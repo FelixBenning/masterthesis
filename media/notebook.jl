c787e403ffd6c20d43c231e5657bebadd06eb589
Felix Benning <felix.benning@gmail.com>
2021-07-13 15:37:53 +0200

    improve max_step calculation

--- media/notebook.jl
+++ media/notebook.jl
@@ -46,12 +46,12 @@ md"# Visualize Gradient Decent"
 default_ev = [
 	Dict(
 		"saddlepoint" => -1,
-		"bad_conditioning" => 0.1,
+		"bad_conditioning" => 0.2,
 		"normal"=> 1
 	)[viz_type],
 	Dict(
 		"saddlepoint" => 2,
-		"bad_conditioning" => 3,
+		"bad_conditioning" => 2,
 		"normal"=> 2
 	)[viz_type]
 		
@@ -89,6 +89,19 @@ begin
 	plot!([0,0], [0,0], quiver=(eigen[1,:], eigen[2,:]), st=:quiver)
 end
 
+# ╔═╡ fa713758-2713-4446-aa5c-f5fa1e2f601c
+md"
+#### Learning Rate (Optimal: $(opt_lr=round(2/(abs(lambda_1)+abs(lambda_2)), digits=2)))
+$(@bind lr Slider(
+	0.01:0.01:round(2/max(abs(lambda_1),abs(lambda_2)), digits=2), 
+	default=opt_lr, 
+	show_value=true
+))
+"
+
+# ╔═╡ c5d4b530-3e8f-4a46-888f-22b54b4be7e2
+log_rates = log.(abs.(1 .-lr.*[lambda_1,lambda_2])) 
+
 # ╔═╡ f76998fa-a01e-4f5f-b714-2440a2f8dd50
 start = Dict(
 	"saddlepoint" => 4*eigenvec2 + 0.001*eigenvec1,
@@ -96,64 +109,58 @@ start = Dict(
 	"normal"=> 3*eigenvec1 + 3*eigenvec2
 )[viz_type]
 
-# ╔═╡ fa713758-2713-4446-aa5c-f5fa1e2f601c
-md"
-#### Learning Rate
-$(@bind lr Slider(
-	0.01:0.01:round(
-		2/max(abs(lambda_1),abs(lambda_2)), 
-		digits=2
-	), 
-	default=round(2/(abs(lambda_1)+abs(lambda_2)), digits=2), 
-	show_value=true
-))
-"
-
 # ╔═╡ 342ee539-d5da-43a6-b7ec-7717c338207c
-max_steps = floor(
-	Int32,
-	min(
-		lambda_1 < 0 ? log(5/(eigenvec1'*start))/log(1- lr*lambda_1) : 1000,
-		lambda_2 < 0 ? log(5/(eigenvec2'*start))/log(1- lr*lambda_2) : 1000
-	)
-)
+max_steps = 
+if maximum(log_rates) > 0
+	# not convex, stop iterate from moving outside frame
+	floor(Int32, minimum(log.(5 ./[eigenvec1'*start,eigenvec2'*start])./log_rates))
+else
+	# convex stop once all are close to 0 (below threshold 0.01)
+	ceil(Int32, maximum(log.(0.01 ./[eigenvec1'*start, eigenvec2'*start])./log_rates))
+end
 
 # ╔═╡ 9bcec655-65b9-4444-b6ab-38ab8ab5d169
 md"#### Gradient Steps
 $(@bind steps Slider(1:max_steps, default=max_steps, show_value=true))
 "
 
+# ╔═╡ 02bfcd93-3512-4d75-9bb2-021fbf226a31
+md"### Gradient Decent Steps"
+
 # ╔═╡ e12b1bcb-4bbf-4adb-b277-c5703486ec72
 begin
 	decent_steps = Array{Float32}(undef, 2, steps)
 	decent_steps[:,1] = start
 	for step in 2:steps
-		decent_steps[:,step] = decent_steps[:,step-1] - lr .* gradient(f, decent_steps[:, step-1])[1]
+		decent_steps[:,step] = 
+		decent_steps[:,step-1] - lr .* gradient(f, decent_steps[:, step-1])[1]
 	end
 	decent_steps
 end
 
 # ╔═╡ f15c0419-96ec-4b9f-afab-00e4e8883146
 begin
-	losssurface = plot(-5:0.1:5, -5:0.1:5, f, st=:contour, title="Losssurface")
+	losssurface = plot(-3:0.1:6, -6:0.1:3, f, st=:contour, title="Losssurface")
 	plot!(
 		losssurface,
 		decent_steps[1,:], 
 		decent_steps[2,:],
 		markershape=:circle,
-		arrow=:arrow,
 		label="Path of Gradient Decent",
 		# aspect_ratio=1
 	)
-	loss = plot(
-		mapslices(f, decent_steps;dims=(1))', 
+	loss = mapslices(f, decent_steps; dims=(1))'
+	lossplot = plot(
+		loss, 
 		st=:scatter, 
 		label=missing, 
 		title="Loss", 
 		xlabel="Iteration",
+		xlim=(0, max_steps+1),
+		ylim=(0-0.2, maximum(loss)+0.2)
 	)
 	gd_viz = plot(
-		losssurface, loss, 
+		losssurface, lossplot, 
 		layout=grid(1,2, widths=[0.7,0.3]), 
 		fontfamily="Computer Modern"
 	)
@@ -162,22 +169,32 @@ end
 # ╔═╡ 45306bbd-ccfa-43bc-a90c-1c1b88f28a46
 savefig(gd_viz, "visualize_$(viz_type).svg")
 
+# ╔═╡ 2d3d6cdf-42f5-4c51-b3fe-58f227bb5aac
+md"### GD with Momentum Steps"
+
+# ╔═╡ 6954f075-9818-4742-98b5-f7e95a85cf77
+md"# Appendix"
+
 # ╔═╡ Cell order:
-# ╠═d58ccd7f-7168-46ad-accb-475f283478e4
-# ╠═47e61ea1-5bf5-4ff0-81a1-f9471ac933f1
 # ╟─f709e1de-aaac-4eb0-a2e0-a2bfa6ae6370
-# ╟─e771e800-f72c-46db-b80d-598c309f9743
+# ╠═e771e800-f72c-46db-b80d-598c309f9743
 # ╟─9e5a1fdd-30de-436e-9090-6142494617b8
 # ╟─6ed6aa03-88e1-4e8a-9bfc-60e3d22058e5
-# ╟─c3e4e730-d97d-11eb-0340-1d1e2fd0167a
+# ╠═c3e4e730-d97d-11eb-0340-1d1e2fd0167a
 # ╟─dc609f58-f607-4772-bdb0-f2db25475e18
 # ╠═3d9d041e-5d53-410f-a1ce-e63b264ea924
 # ╟─491bbd1c-39f6-4631-a9e0-1e29dbcc4ec1
 # ╟─5993f2a2-dd65-49a5-8777-83ea02194f89
 # ╟─f15c0419-96ec-4b9f-afab-00e4e8883146
-# ╟─f76998fa-a01e-4f5f-b714-2440a2f8dd50
 # ╠═45306bbd-ccfa-43bc-a90c-1c1b88f28a46
 # ╟─fa713758-2713-4446-aa5c-f5fa1e2f601c
+# ╟─c5d4b530-3e8f-4a46-888f-22b54b4be7e2
 # ╟─342ee539-d5da-43a6-b7ec-7717c338207c
 # ╟─9bcec655-65b9-4444-b6ab-38ab8ab5d169
+# ╟─f76998fa-a01e-4f5f-b714-2440a2f8dd50
+# ╟─02bfcd93-3512-4d75-9bb2-021fbf226a31
 # ╠═e12b1bcb-4bbf-4adb-b277-c5703486ec72
+# ╟─2d3d6cdf-42f5-4c51-b3fe-58f227bb5aac
+# ╟─6954f075-9818-4742-98b5-f7e95a85cf77
+# ╠═d58ccd7f-7168-46ad-accb-475f283478e4
+# ╠═47e61ea1-5bf5-4ff0-81a1-f9471ac933f1
